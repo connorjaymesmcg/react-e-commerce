da15c213a8f070d2fe6d2956555ae62a5717a298
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-12-30 14:42:30 -0500

    basic component folder structureadded

---
+++ src/Components/Products/Product/Product.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = ({ product }) => {
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image='' title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant='h5' gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant='h5' gutterBottom>
+            {product.name}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Product;
