a8a1c0fc02daf4ec6d643d9f30d115236290ea3a
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-12-30 17:57:34 -0500

    added chec API key to env

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,15 +3,20 @@ import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
 
+import useStyles from './productsStyles';
+
 const products = [
   { id: 1, name: 'Shoes', description: 'Skate shoes', price: '$5', image: 'https://picsum.photos/id/0/200/300' },
   { id: 2, name: 'MacBook Pro', description: 'Apple MacBook', price: '$3000', image: 'https://picsum.photos/id/0/200/300' },
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
-      <Grid container justifyContent='center' spacing={4}>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justifyContent='center' spacing={2}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} />
