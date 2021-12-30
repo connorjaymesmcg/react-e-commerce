75d3e63409f27521013d5d1ed078578302e12c9d
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2021-12-30 16:28:43 -0500

    Placeholder elements complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import Navbar from './Components/Navbar/Navbar';
+// import Products from './Components/Products/Products';
+
+import { Products, Navbar } from './Components';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Products />
+    </div>
+  );
+};
+
+export default App;
