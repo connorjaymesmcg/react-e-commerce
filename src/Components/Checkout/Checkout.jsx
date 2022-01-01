c6179163e3ffc29a81860e640858f2b2d59b9b00
Connor McGinnis <connormcginnis@Connors-MacBook-Pro.local>
2022-01-01 15:04:21 -0500

    imported materialUI components into Checkout component

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+
+import useStyles from './checkoutStyles';
+
+const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
 
-export default Checkout
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.Paper}>
+          <Typography variant='h4' align='center'>Checkout</Typography>
+          <Stepper activeStep={0} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
